f4dbf69f3585f7abfecdc47abad569b9a1909965
Bioparco90 <monnimarco90@gmail.com>
2024-04-22 11:53:46 +0200

    Start implementing ICrud interface. Methods Get and GetAll done

---
+++ src/DataAccessLayer.Library/DataHandler.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Data;
+
+namespace DataAccessLayer.Library
+{
+    public class DataHandler<T> : ICrud<T>
+    {
+        private readonly DataTableAccess<T> Table = new();
+
+        public bool Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? Get(T item) => GetAll().SingleOrDefault(item);
+
+        public IEnumerable<T> GetAll()
+        {
+            DataSet dataset = Table.ReadDataSetFromFile(Table.XMLFileName);
+            var result = Table.ConvertDataSetToList(dataset);
+            return result;
+        }
+
+        public bool Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/DataAccessLayer.Library/ICrud.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccessLayer.Library
+{
+    internal interface ICrud<T>
+    {
+        public bool Add(T item);
+        public T? Get(T item);
+        public IEnumerable<T> GetAll();
+        public bool Update(T item);
+        public bool Delete(T item);
+    }
+}
