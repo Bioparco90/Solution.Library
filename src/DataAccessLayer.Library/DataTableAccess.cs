3f0e9d3e1c20d31cacde89704c1b7f5e7c759399
Bioparco90 <monnimarco90@gmail.com>
2024-04-23 11:29:01 +0200

    GetProperties logic resetted

--- src/DataAccessLayer.Library/DataTableAccess.cs
+++ src/DataAccessLayer.Library/DataTableAccess.cs
@@ -5,18 +5,12 @@ namespace DataAccessLayer.Library
 {
 
     // TODO: evitare duplicati
-    // TODO: valutare il senso di avere un dataset, datatable dovrebbe bastare
     public class DataTableAccess<T>
     {
         public string Extension => ".xml";
         public string ClassType => typeof(T).Name;
         public string XMLFileName => $"{ClassType}{Extension}";
-
-        private List<string> ExcludedProperties = ["RoleEnum"];
-        private PropertyInfo[] Properties => typeof(T).GetProperties()
-            .Where(item => !ExcludedProperties.Contains(item.Name))
-            .Cast<PropertyInfo>()
-            .ToArray();
+        private PropertyInfo[] Properties => typeof(T).GetProperties();
 
         public DataTable AddItemToDataTable(T item)
         {
