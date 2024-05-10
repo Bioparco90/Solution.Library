69fb5ceac051faf9f675e2e459f26f5db9bba96f
Bioparco90 <monnimarco90@gmail.com>
2024-05-10 15:10:32 +0200

    .

--- src/ConsoleApp.Library/Program.cs
+++ src/ConsoleApp.Library/Program.cs
@@ -1,5 +1,3 @@
 ﻿Console.WriteLine("Don't delete me");
 
-// TODO: Con la pseudo-sessione possiamo salvare lo user o lo userId, ci sarà da rivedere il passaggio di alcuni parametri in alcuni metodi
 // TODO: classe SearchBookResult per mostrare l'anagrafica, la prenotabilità del libro e la data in cui sarà prenotabile (se disponibile, today)
-// TODO: Sistemare campi mandatory come da specifiche
