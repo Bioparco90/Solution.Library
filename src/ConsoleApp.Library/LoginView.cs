bea5b254a76a3d4d283f29c47d04de3759080fbd
Bioparco <monnimarco90@gmail.com>
2024-05-12 11:47:46 +0200

    Done

--- src/ConsoleApp.Library/LoginView.cs
+++ src/ConsoleApp.Library/LoginView.cs
@@ -17,9 +17,33 @@
         public LoginRecord AskLogin()
         {
             string username = _menuUtils.GetStrictInteraction("Username", CheckEmpty);
-            string password = _menuUtils.GetStrictInteraction("Password", CheckEmpty);
-            
+            string password = _menuUtils.GetStrictInteraction("Password", CheckEmpty, ReadPassword);
+
             return new(username, password);
+        }
+
+        private string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo key;
+
+            do
+            {
+                key = Console.ReadKey(true); // true per non mostrare il tasto premuto
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    password += key.KeyChar;
+                    Console.Write("*"); // Visualizza un carattere di oscuramento
+                }
+                else if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    password = password.Remove(password.Length - 1);
+                    Console.Write("\b \b"); // Cancella l'ultimo carattere visualizzato
+                }
+            } while (key.Key != ConsoleKey.Enter);
+
+            Console.WriteLine();
+            return password;
         }
     }
 }
