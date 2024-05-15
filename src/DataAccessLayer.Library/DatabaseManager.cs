49c57873d352efc84653ef9109b44bd694cd60bf
Bioparco <monnimarco90@gmail.com>
2024-05-15 20:00:52 +0200

    New Data Access done

---
+++ src/DataAccessLayer.Library/DatabaseManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.SqlClient;
+
+namespace DataAccessLayer.Library
+{
+    public class DatabaseManager
+    {
+        public readonly string _connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True;";
+
+        public string CreateString(Dictionary<string, object> parameters)
+        {
+            List<string> k = new();
+            foreach (KeyValuePair<string, object> kvp in parameters)
+            {
+                k.Add($" {kvp.Key}=@{kvp.Key.ToLower()}");
+            }
+
+            return string.Join(",", k);
+        }
+
+        public string CreateFilterString(Dictionary<string, object> parameters)
+        {
+            List<string> k = new();
+            foreach (KeyValuePair<string, object> kvp in parameters)
+            {
+                k.Add($" {kvp.Key}=@{kvp.Key.ToLower() + "Filter"}");
+            }
+
+            return string.Join(" AND", k);
+        }
+
+        public void AddParameters(SqlCommand cmd, Dictionary<string, object> parameters)
+        {
+            foreach (KeyValuePair<string, object> kvp in parameters)
+            {
+                cmd.Parameters.AddWithValue(kvp.Key.ToLower(), kvp.Value);
+            }
+        }
+
+        public void AddFilterParameters(SqlCommand cmd, Dictionary<string, object> parameters)
+        {
+            foreach (KeyValuePair<string, object> kvp in parameters)
+            {
+                cmd.Parameters.AddWithValue(kvp.Key + "Filter", kvp.Value);
+            }
+        }
+    }
+}
