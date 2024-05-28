27d230ff528fff272fb4d15f04c6a94e00986493
Bioparco <monnimarco90@gmail.com>
2024-05-28 23:06:57 +0200

    ReservationHistory helpers was the same in admin and user views.

--- src/ConsoleApp.Library/Views/UserView.cs
+++ src/ConsoleApp.Library/Views/UserView.cs
@@ -30,47 +30,5 @@ namespace ConsoleApp.Library.Views
 
             return reservations.Any();
         }
-
-        protected override void BookFilterLoop(ref IEnumerable<HumanReadableReservation> reservations)
-        {
-            AskFilterMode("Would you like to search by book?");
-            string choice = _utils.GetStrictInteraction("Insert command", input => _utils.CheckInputChoice(input, 2));
-
-            if (choice == "1")
-            {
-                bool endLoop = false;
-                do
-                {
-                    var book = BuildBook(BusinessLogic.Library.Enums.Method.Get);
-                    try
-                    {
-                        var found = _bookHandler.SearchSingle(book, parametersCount => parametersCount == 4);
-                        reservations = reservations.Where(r => r.Title == found.Title);
-                        endLoop = true;
-                    }
-                    catch (InvalidOperationException)
-                    {
-                        Console.WriteLine("The search is ambiguous, please be more specific.");
-                        endLoop = !AskToContinue("book");
-                    }
-                }
-                while (!endLoop);
-            }
-        }
-
-        protected override void StatusFilter(ref IEnumerable<HumanReadableReservation> reservations)
-        {
-            AskFilterMode("Would you like to search by status (active/expired)?");
-            string choice = _utils.GetInteraction("Insert command (default: NO): ");
-
-            if (choice == "1")
-            {
-                Console.WriteLine("1. Active");
-                Console.WriteLine("2. Expired");
-                choice = _utils.GetStrictInteraction("Insert command", input => _utils.CheckInputChoice(input, 2));
-                string status = choice == "1" ? "Active" : "Expired";
-                reservations = reservations.Where(r => r.Status == status);
-            }
-        }
     }
 }
